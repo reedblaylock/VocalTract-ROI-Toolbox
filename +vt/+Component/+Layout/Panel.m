9756a61839115fab16d2aa0bea5df3f882cbf41e
Reed Blaylock <reed.blaylock@gmail.com>
2017-08-25 17:30:12 -0700

    Add legacy cababilities to Panel and TabPanel.

--- +vt/+Component/+Layout/Panel.m
+++ +vt/+Component/+Layout/Panel.m
@@ -2,6 +2,25 @@ classdef Panel < vt.Component.Layout
 	methods
 		function this = Panel(parent, varargin)
 			this@vt.Component.Layout(parent, varargin{:});
+        end
+        
+        function [] = setParameters(this, varargin)
+			p = vt.InputParser;
+			p.KeepUnmatched = true;
+			p.addRequired('this', @(this) isa(this, 'vt.Component.Layout'));
+			parse(p, this, varargin{:});
+			
+			s = p.Unmatched;
+			
+			if( this.isOldMatlabVersion() )
+                % Change Widths to Sizes
+				nameToChange = 'Contents';
+				nameReplacement = 'Children';
+				s = this.renameField(s, nameToChange, nameReplacement);
+			end
+			
+			c = this.struct2interleavedCell(s);
+			setParameters@vt.Component.Layout(this, c{:});
 		end
 	end
 	
--- +vt/+Component/+Layout/TabPanel.m
+++ +vt/+Component/+Layout/TabPanel.m
@@ -2,6 +2,45 @@ classdef TabPanel < vt.Component.Layout
 	methods
 		function this = TabPanel(parent, varargin)
 			this@vt.Component.Layout(parent, varargin{:});
+        end
+        
+        function [] = setParameters(this, varargin)
+			p = vt.InputParser;
+			p.KeepUnmatched = true;
+			p.addRequired('this', @(this) isa(this, 'vt.Component.Layout'));
+			parse(p, this, varargin{:});
+			
+			s = p.Unmatched;
+			
+			if( this.isOldMatlabVersion() )
+				% Change Widths to Sizes
+				nameToChange = 'TabWidth';
+				nameReplacement = 'TabSize';
+				s = this.renameField(s, nameToChange, nameReplacement);
+                
+                % Change Widths to Sizes
+				nameToChange = 'TabEnables';
+				nameReplacement = 'TabEnable';
+				s = this.renameField(s, nameToChange, nameReplacement);
+                
+                % Change Widths to Sizes
+				nameToChange = 'Contents';
+				nameReplacement = 'Children';
+				s = this.renameField(s, nameToChange, nameReplacement);
+                
+                % Change Widths to Sizes
+				nameToChange = 'TabTitles';
+				nameReplacement = 'TabNames';
+				s = this.renameField(s, nameToChange, nameReplacement);
+                
+                % Change Widths to Sizes
+				nameToChange = 'Selection';
+				nameReplacement = 'SelectedChild';
+				s = this.renameField(s, nameToChange, nameReplacement);
+			end
+			
+			c = this.struct2interleavedCell(s);
+			setParameters@vt.Component.Layout(this, c{:});
 		end
 	end
 	
