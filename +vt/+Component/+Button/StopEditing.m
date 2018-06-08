639b1bd7bb5566e7d5155c0a6b4b6b64ce488578
Reed Blaylock <reed.blaylock@gmail.com>
2018-06-08 08:06:16 -0400

    Bug fix: ability to stop editing a region that hasn't been placed leaves floating regions
    Bug: the "Stop Editing" button is available even when regions don't have an origin yet. If the button is pressed and the region doesn't have an origin, the region will not be click-able for future edits, and will cause problems with timeseries exports (probably).
    
    Fix: if "Stop Editing" is pressed when the region origin has not been set, delete the region instead.

--- +vt/+Component/+Button/StopEditing.m
+++ +vt/+Component/+Button/StopEditing.m
@@ -1,4 +1,8 @@
 classdef StopEditing < vt.Component.Button & vt.State.Listener & vt.Action.Dispatcher
+	properties
+		currentRegion
+	end
+	
 	methods
 		function this = StopEditing(parent, label, varargin)
 			this@vt.Component.Button(parent, label, varargin{:});
@@ -15,9 +19,25 @@ classdef StopEditing < vt.Component.Button & vt.State.Listener & vt.Action.Dispa
 			end
 		end
 		
+		function [] = onCurrentRegionChange(this, state)
+			regions = state.regions;
+			for iRegion = 1:numel(regions)
+				if regions{iRegion}.id == state.currentRegion
+					this.currentRegion = regions{iRegion};
+					break;
+				end
+			end
+		end
+		
 		function [] = dispatchAction(this, source, eventData)
-			action = this.actionFactory.actions.STOP_EDITING;
-			action.prepare();
+			if isempty(this.currentRegion.origin)
+				action = this.actionFactory.actions.DELETE_REGION;
+				action.prepare(this.currentRegion.id);
+			else
+				action = this.actionFactory.actions.STOP_EDITING;
+				action.prepare();
+			end
+			
 			action.dispatch();
 		end
 	end
