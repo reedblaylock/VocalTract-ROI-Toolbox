f25425b6da00905c2995325df07607005457aa57
Reed Blaylock <reed.blaylock@gmail.com>
2018-06-15 08:02:03 -0400

    Feature: automatic region color changing
    Before: every region started out as a default "red"
    
    After: each region gets its own color
    
    TODO: some colors don't show up well in grayscale

--- +vt/+Action/NewRegion.m
+++ +vt/+Action/NewRegion.m
@@ -5,11 +5,9 @@ classdef NewRegion < vt.Action
 	
 	properties
 		region
-		id = 0
+% 		id = 0
 	end
 	
-	% TODO: When you load states from a previous session, this will overwrite
-	% IDs.
 	methods (Static, Access = private)
 		function out = getOrIncrementCount(increment)
 			persistent id;
@@ -23,27 +21,27 @@ classdef NewRegion < vt.Action
 		end
 	end
 	
-	methods
-% 		function out = getOrIncrementCount(this, increment)
-% 			p = inputParser;
-% 			addOptional(p, 'increment', 0, @isnumeric);
-% 			parse(p, increment);
-% 			
-% 			if increment
-% 				this.id = this.id + p.Results.increment;
-% 			end
-% 			
-% 			out = this.id;
-% 		end
-		
+	methods		
 		function [] = prepare(this)
 			config = vt.Config();
 			this.region = config.region;
 			
 			% Increment the counter in the constructor
 			this.region.id = vt.Action.NewRegion.getOrIncrementCount(1);
-% 			this.region.id = this.getOrIncrementCount(1);
 			this.region.name = ['Region' num2str(this.region.id)];
+			this.region.color = this.getNextColor();
+		end
+		
+		function color = getNextColor(this)
+			% Adopted from:
+			% https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+			% Doesn't give as much color dispersion as you'd like
+			golden_ratio_conjugate = 0.618033988749895;
+% 			h = rand;
+% 			h = h + golden_ratio_conjugate;
+			h = golden_ratio_conjugate * vt.Action.NewRegion.getOrIncrementCount();
+			h = mod(h, 1);
+			color = hsv2rgb(h, 0.8, 0.95);
 		end
 		
 		function [] = delete(this)
