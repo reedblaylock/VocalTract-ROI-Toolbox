ed0f5d546842ee6e1f6163cefd0efbeb4e6dfb40
Reed Blaylock <reed.blaylock@gmail.com>
2018-06-16 15:26:07 -0400

    Add Layout.BoxPanel class

---
+++ +vt/+Component/+Layout/BoxPanel.m
@@ -0,0 +1,58 @@
+classdef BoxPanel < vt.Component.Layout
+	methods
+		function this = BoxPanel(parent, varargin)
+			this@vt.Component.Layout(parent, varargin{:});
+        end
+        
+        function [] = setParameters(this, varargin)
+			p = inputParser;
+			p.KeepUnmatched = true;
+			p.addRequired('this', @(this) isa(this, 'vt.Component.Layout'));
+			parse(p, this, varargin{:});
+			
+			s = p.Unmatched;
+			
+			if vt.Config.isOldMatlabVersion()
+                % Change Contents to Children
+				nameToChange = 'Contents';
+				nameReplacement = 'Children';
+				s = this.renameField(s, nameToChange, nameReplacement);
+				
+				% Change Selection to SelectedChild
+				nameToChange = 'Selection';
+				nameReplacement = 'SelectedChild';
+				s = this.renameField(s, nameToChange, nameReplacement);
+				
+				% Change Docked to isDocked
+				nameToChange = 'Docked';
+				nameReplacement = 'isDocked';
+				s = this.renameField(s, nameToChange, nameReplacement);
+			end
+			
+			c = this.struct2interleavedCell(s);
+			setParameters@vt.Component.Layout(this, c{:});
+		end
+	end
+	
+	methods (Access = protected)
+		function [] = construct(this, parent)
+			p = vt.InputParser;
+			p.addParent();
+			parse(p, parent);
+			
+% 			this.handle = uiextras.Panel( ...
+% 				'Parent', p.Results.parent.handle ...
+% 			);
+			if vt.Config.isOldMatlabVersion()
+				this.handle = uiextras.BoxPanel( ...
+					'Parent', p.Results.parent.handle ...
+				);
+			else
+				this.handle = uix.BoxPanel( ...
+					'Parent', p.Results.parent.handle ...
+				);
+			end
+		end
+	end
+end
+
